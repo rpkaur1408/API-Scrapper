805aeaa10995ab8d3d13949cdd7e46752216ca1a
Rehatpreet kaur <rehatpreetkaur981@gmail.com>
2024-12-19 11:01:36 -0500

    Getting list of quotes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const puppeteer = require("puppeteer");
 
+
 const getQuotes = async () => {
   // Start a Puppeteer session with:
   // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
-  // - no default viewport (`defaultViewport: null` - website page will in full width and height)
+  // - no default viewport (`defaultViewport: null` - website page will be in full width and height)
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
@@ -18,6 +19,30 @@ const getQuotes = async () => {
   await page.goto("http://quotes.toscrape.com/", {
     waitUntil: "domcontentloaded",
   });
+
+  // Get page data
+  const quotes = await page.evaluate(() => {
+    // Fetch the first element with class "quote"
+    // Get the displayed text and returns it
+    const quoteList = document.querySelectorAll(".quote");
+
+    // Convert the quoteList to an iterable array
+    // For each quote fetch the text and author
+    return Array.from(quoteList).map((quote) => {
+      // Fetch the sub-elements from the previously fetched quote element
+      // Get the displayed text and return it (`.innerText`)
+      const text = quote.querySelector(".text").innerText;
+      const author = quote.querySelector(".author").innerText;
+
+      return { text, author };
+    });
+  });
+
+  // Display the quotes
+  console.log(quotes);
+
+  // Close the browser
+  await browser.close();
 };
 
 // Start the scraping
